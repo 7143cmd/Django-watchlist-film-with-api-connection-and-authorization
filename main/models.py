0e775e8cf78c2c589762e48c0756c3bde85177dd
Dmytro <pytakdima@gmail.com>
2026-04-04 19:24:27 +0300

    DB with user info added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class User_Data(models.Model):
+    nickname = models.CharField('Nickname', max_length = 20)
+    email = models.CharField('email', max_length = 30)
+    password = models.CharField('password', max_length = 16)
+
+    def __str__(self):
+        return self.nickname
+    
+#class Watchlist(models.Model):
+
 # Create your models here.
+
