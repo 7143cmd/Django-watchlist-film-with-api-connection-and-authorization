bff06be005d283246123b8413c7d6b75ef41e90b
Dmytro <pytakdima@gmail.com>
2026-04-06 16:37:56 +0300

    watchlist model added

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class CustomUser(AbstractUser):
@@ -9,7 +10,20 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-#class Watchlist(models.Model):
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='watchlist'
+    )
+    movie = models.CharField(max_length=9)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (f"{self.user} - {self.movie}")
+
 
 # Create your models here.
 
